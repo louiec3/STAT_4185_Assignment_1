b3de5a310a354de0669566f6acb4f286c10dce8e
Louis Cundari III <louiscundari3@outlook.com>
2023-09-10 23:02:42 -0400

    Script to decrypt the second hidden message

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -1,7 +1,26 @@
-encrypted_file = open("encrypted_message_two.txt", 'r')
-
-encrypted_message = encrypted_file.readline()
-
-encrypted_file.close()
+with open("encrypted_message_two.txt", 'r') as f:
+    encrypted_message = f.readline()
 
 # Write Code Here
+
+# Make a list of the even indexes of the encrypyted text
+# Make a list of the odd indexes of the encrypyted text
+# Reverse the odd index list
+even_index = encrypted_message[::2]
+odd_index_rev = encrypted_message[1::2][::-1]
+
+decrypted_message = ''
+x = 0
+
+# Loop elements and append the even_index list then the odd_index_reverse to 
+# build the message.
+for i in enumerate(encrypted_message):
+    if x < len(odd_index_rev):
+        decrypted_message += even_index[x]
+        decrypted_message += odd_index_rev[x]
+        x+=1
+    else:
+        decrypted_message += even_index[x]
+        break
+
+print(decrypted_message)
